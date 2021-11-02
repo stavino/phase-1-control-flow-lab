1d55a60795ef16b7aed7c9a5a68cdf497b90f5ab
Stephen Avino <stephen.avino@gmail.com>
2021-11-01 22:41:43 -0400

    commit tree 6b076174716c062ade920db8f85ff071f426890b

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if(someValue <= 400){
+    return `This one is on me!`;
+  }
+  if(someValue > 2000){
+    return someValue > 2500 ? `No can do.` : `I will gladly take your thirty bucks.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === `NYC`){
+  return `Ok, sounds good.`;
+  }
+  else{
+  return `No go.`;
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+let response;
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case `generous`:
+      return `Thank you so much.`;
+    case `not as generous`:
+      return `Thank you.`;
+    default:
+      return `Bye.`;
+  }
 }
